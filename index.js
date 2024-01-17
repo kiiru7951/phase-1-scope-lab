0c82b806a9f138328c2ec35c86a66ea014e69a0a
Allan <allan.kariuki@student.moringaschool.com>
2024-01-17 14:26:23 +0300

    Done :)

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+var setBestCustomer = () => {
+    bestCustomer = 'not bob';
+};
+
+var overwriteBestCustomer = newFavorite => {
+    bestCustomer = newFavorite;
+};
+
+const leastFavoriteCustomer = 'mike';
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'someone else';
+};
